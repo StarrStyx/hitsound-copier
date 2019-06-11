e2f80a0ab06e85c4a48a62daf6ecbab34c8497dc
Adrian <starrstyx@users.noreply.github.com>
2019-06-12 00:14:43 +0800

    Basic functionality
    Read files
    Extract timing point and hit object data

--- main.py
+++ main.py
@@ -1,24 +1,62 @@
-# imports?
+# yea idk wot Im doing
+import os
 
-# reading the file
-# both files, in fact
+# input for osu filename
+# mapfile = input("Target .osu filename: ")
+# mapfile = input("Hitsounded .osu filename: ")
 
-# print("Please enter directory of .osu file")
-in_input = input("Please enter directory of .osu file to copy from:")
-out_input = input("Please enter directory of .osu file to copy to:")
+###########################################################################
 
-print(in_input)
-print(out_input)
+# reading the file: testing with .osu
+mapfileinput = "testfiles\\bios_hs.osu"
+mapfilehitsound = "testfiles\\bios_nohs.osu"
+mapfilebackup = mapfileinput + ".bak"
+asdf = "debug.txt"
 
-in_osu = open("testing/one.osu","r")
-out_osu = open("testing/two.osu","w")
+# reading the file: testing with .txt
+# mapfileinput = "testfiles\\in.txt"
+# mapfilehitsound = "testfiles\\original.txt"
+# mapfilebackup = mapfileinput + ".bak"
 
-# determines hitsounds to add
+##########################################################################
 
-# puts them back in line?
+# read mode for specified files
+input = open(mapfileinput, "r")
+hitsound = open(mapfilehitsound, "r")
 
-# debug section
-print(in_osu.read())
+# write mode for specified files
+backup = open(mapfilebackup, "w")
+debug_timingpoints = open(asdf, "w")
 
-out_osu.write("yea lol")
-out_osu.close()
+##########################################################################
+
+# takes in .osu as string
+mapdata = input.read()
+
+# print(mapdata)
+
+# backup input .osu
+backup.write(mapdata)
+
+##########################################################################
+
+# takes timing points
+timingpoints = mapdata.partition("[TimingPoints]")[2].partition("[HitObjects]")[0]
+
+# takes hitobjects
+hitobjects = mapdata.partition("[HitObjects]")[2]
+
+print(timingpoints)
+print(hitobjects)
+
+# debug
+print(mapfileinput)
+print(mapfilebackup)
+print(os.path.basename(mapfilebackup))
+
+print(input.name)
+# print(info)
+
+# replaces original osu
+# output = open(mapfileinput, "w")
+# aaaa = debug_timingpoints.write(timingpoints)
